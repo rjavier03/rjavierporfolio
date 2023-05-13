86809ec77addffde36f4667cf32307db76969224
rjavier03 <84501536+rjavier03@users.noreply.github.com>
2023-05-13 14:04:38 +0800

    Initial Commit
    Layout and some animation

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var hide = document.getElementById("hide-side")
+
+hide.addEventListener("click", function() {
+    var sidebar = document.getElementById("side-bar")
+    var content = document.getElementById("content-container")
+  
+    if(hide.innerHTML == "HideSideBar()"){
+        sidebar.classList.add("hide-side-bar");
+      
+        content.classList.add("expanded-content")
+        hide.innerHTML = "ViewSideBar()";
+    }
+    else{
+     
+        setTimeout(function(){
+            sidebar.classList.remove("hide-side-bar");
+
+        }, 300);
+        content.classList.remove("expanded-content")
+        hide.innerHTML = "HideSideBar()";
+    }
+  
+
+  });
